04d6f5d37afd198782ca0683e55b4e4b61fd81c9
chip1991 <972339179@qq.com>
2026-04-08 16:29:53 +0000

    fix: resolve process is not defined error using vite-plugin-node-polyfills

--- doocs-md-new/vite.config.ts
+++ doocs-md-new/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['process', 'buffer', 'path', 'util', 'stream'],
+      globals: {
+        process: true,
+        Buffer: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
